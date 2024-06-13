0ca1a92046e46d19508aef54006f398f7e504cc2
Andy Wright <atwright147@gmail.com>
2024-06-13 17:14:50 +0100

    list files on disk

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct FileInfo {
+    path: String,
+    is_file: bool,
+    size: Option<u64>,
+}
+
+#[tauri::command]
+fn list_files(drive_path: String) -> Result<Vec<FileInfo>, String> {
+  println!("Received drive path: {}", drive_path);
+
+  // Check if path exists
+  if !std::path::Path::new(&drive_path).exists() {
+    return Err(format!("Path does not exist: {}", drive_path));
+  }
+
+  let mut files = Vec::new();
+  // Iterate over entries in the provided drive_path
+  for entry in walkdir::WalkDir::new(drive_path).into_iter().filter_map(|e| e.ok()) {
+    let metadata = entry.metadata().map_err(|err| err.to_string())?;
+
+    files.push(FileInfo {
+      path: entry.path().display().to_string(),
+      is_file: metadata.is_file(),
+      size: if metadata.is_file() { Some(metadata.len()) } else { None },
+    });
+  }
+
+  Ok(files)
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +42,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![list_files, greet])
     .plugin(tauri_plugin_system_info::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import type { FileEntry } from '@tauri-apps/api/fs';
 import { useEffect, useMemo, useState } from 'react';
 import Select from 'react-select';
-
 import { AllSystemInfo, allSysInfo } from 'tauri-plugin-system-info-api';
 import type { Disk } from 'tauri-plugin-system-info-api';
 
 import './App.css';
+import { invoke } from '@tauri-apps/api';
+
+type FileInfo = {
+  path: string;
+  is_file: boolean;
+  size?: number;
+};
 
 function App() {
   const [disk, setDisk] = useState('');
+  const [files, setFiles] = useState<FileEntry[]>([]);
   const [removableDisks, setRemovableDisks] = useState<Disk[]>([]);
 
   const options = useMemo(() => removableDisks.map((disk) => ({ value: disk.mount_point, label: disk.name })), [removableDisks]);
@@ -15,10 +23,32 @@ function App() {
   useEffect(() => {
     (async () => {
       const disks = await getRemovableDisks();
-      const removableDisks = disks.filter((disk) => disk.is_removable);
+      const removableDisks = disks; //.filter((disk) => disk.is_removable);
+      // const removableDisks = disks;
       setRemovableDisks(removableDisks);
     })();
   }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if (disk) {
+  //       const files = await readDir(disk);
+  //       setFiles(files);
+  //       console.log(files);
+  //     }
+  //   })();
+  // }, [disk]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result: FileInfo[] = await invoke('list_files', { drivePath: disk });
+        setFiles(result);
+      } catch (error) {
+        console.error('Error listing files:', error);
+      }
+    })();
+  }, [disk]);
 
   const getRemovableDisks = async (): Promise<Disk[]> => {
     try {
@@ -31,7 +61,9 @@ function App() {
 
   const handleFocus = async () => {
     const disks = await getRemovableDisks();
-    setRemovableDisks(disks.filter((disk) => disk.is_removable));
+    const removableDisks = disks; //.filter((disk) => disk.is_removable);
+    // const removableDisks = disks;
+    setRemovableDisks(removableDisks);
   };
 
   return (
@@ -61,7 +93,7 @@ function App() {
         />
       </form>
 
-      <pre>{JSON.stringify({ disk, removableDisks }, null, 2)}</pre>
+      <pre>{JSON.stringify({ disk, removableDisks, options, files }, null, 2)}</pre>
     </div>
   );
 }
